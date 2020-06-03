f2eeb9c3d0a58779a89a4bbb5f0cc7a142fed511
harveyaa <annabelle.ahrv@gmail.com>
2020-06-03 12:06:30 -0400

    organizing

--- ptn_script.py
+++ ptn_script.py
@@ -6,6 +6,7 @@ import pandas as pd
 from scipy.stats import pearsonr
 from sklearn.model_selection import LeaveOneOut
 from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error
 
 #mat is shape n_subjectsXn_edges y is shape n_edges
@@ -89,32 +90,149 @@ def leave_one_out_CPM(mat,y,thresh=0.01):
         
         if (i%10 ==0): print('CPM fold: ',i)
         i = i+1
-    return prediction,edge_count
+    
+    MSE = [['combined',mean_squared_error(y,prediction[:,0])],
+            ['positive',mean_squared_error(y,prediction[:,1])],
+           ['negative',mean_squared_error(y,prediction[:,2])],
+           ['multiple_reg',mean_squared_error(y,prediction[:,3])]]
+    
+    return prediction,edge_count,MSE
+
+def leave_one_out_LR(mat,y,thresh=0.01):
+    n_subjects = mat.shape[0]
+    n_edges = mat.shape[1]
+
+    #significant, positive, negative
+    prediction = np.zeros((n_subjects,3))
+    #for each edge number of subjects for which it was significantly correlated
+    edge_count = np.zeros((n_edges,3))
+    
+    loo = LeaveOneOut()
+    i = 0
+    for train_index,test_index in loo.split(mat):
+        mat_train, mat_test = mat[train_index], mat[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        #For each of 200x200 edges across subjects find the correlation with neuroticism score
+        corr = correlate_edges(mat_train,y_train)
+        #Filter edges to significantly correlated (p value below threshold), then into positive and negative correlation
+        sig_edges, pos_edges, neg_edges = filter_edges(corr,thresh)
+        
+        #Create binary masks edges, for each subject count up the values of those edges in their connectivity matrices
+        #equivalent to the dot product between the mask and their connectivity matrix (both flattened)
+        #use all significant edges in summary score
+        significant_edges_train = mat_train[:,sig_edges]
+        significant_edges_test = mat_test[:,sig_edges]
+        #use only positive significant edges
+        positive_edges_train = mat_train[:,pos_edges]
+        positive_edges_test = mat_test[:,pos_edges]
+        #use only negative significant edges
+        negative_edges_train = mat_train[:,neg_edges]
+        negative_edges_test = mat_test[:,neg_edges]
+
+        significant_model = LinearRegression().fit(significant_edges_train,y_train.reshape(-1,1))
+        positive_model = LinearRegression().fit(positive_edges_train,y_train.reshape(-1,1))
+        negative_model = LinearRegression().fit(negative_edges_train,y_train.reshape(-1,1))        
+        
+        #count how many times each edge is significant for each type of edges
+        mask = np.zeros((n_edges,3))
+        mask[sig_edges,0]=1
+        mask[pos_edges,1]=1
+        mask[neg_edges,2]=1
+        edge_count = edge_count + mask
+
+        prediction[i,0]=significant_model.predict(significant_edges_test)
+        prediction[i,1]=positive_model.predict(positive_edges_test)
+        prediction[i,2]=negative_model.predict(negative_edges_test)
+        
+        if (i%10 ==0): print('Fold: ',i)
+        i = i+1
+    
+    MSE = [['significant',mean_squared_error(y,prediction[:,0])],
+            ['positive',mean_squared_error(y,prediction[:,1])],
+           ['negative',mean_squared_error(y,prediction[:,2])]]
+    
+    return prediction,edge_count,MSE
+
+def leave_one_out_SVR(mat,y,thresh=0.01):
+    n_subjects = mat.shape[0]
+    n_edges = mat.shape[1]
+
+    #significant, positive, negative
+    prediction = np.zeros((n_subjects,3))
+    #for each edge number of subjects for which it was significantly correlated
+    edge_count = np.zeros((n_edges,3))
+    
+    loo = LeaveOneOut()
+    i = 0
+    for train_index,test_index in loo.split(mat):
+        mat_train, mat_test = mat[train_index], mat[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        #For each of 200x200 edges across subjects find the correlation with neuroticism score
+        corr = correlate_edges(mat_train,y_train)
+        #Filter edges to significantly correlated (p value below threshold), then into positive and negative correlation
+        sig_edges, pos_edges, neg_edges = filter_edges(corr,thresh)
+        
+        #Create binary masks edges, for each subject count up the values of those edges in their connectivity matrices
+        #equivalent to the dot product between the mask and their connectivity matrix (both flattened)
+        #use all significant edges in summary score
+        significant_edges_train = mat_train[:,sig_edges]
+        significant_edges_test = mat_test[:,sig_edges]
+        #use only positive significant edges
+        positive_edges_train = mat_train[:,pos_edges]
+        positive_edges_test = mat_test[:,pos_edges]
+        #use only negative significant edges
+        negative_edges_train = mat_train[:,neg_edges]
+        negative_edges_test = mat_test[:,neg_edges]
+
+        significant_model = SVR().fit(significant_edges_train,y_train)
+        positive_model = SVR().fit(positive_edges_train,y_train)
+        negative_model = SVR().fit(negative_edges_train,y_train)        
+        
+        #count how many times each edge is significant for each type of edges
+        mask = np.zeros((n_edges,3))
+        mask[sig_edges,0]=1
+        mask[pos_edges,1]=1
+        mask[neg_edges,2]=1
+        edge_count = edge_count + mask
+
+        prediction[i,0]=significant_model.predict(significant_edges_test)
+        prediction[i,1]=positive_model.predict(positive_edges_test)
+        prediction[i,2]=negative_model.predict(negative_edges_test)
+        
+        if (i%10 ==0): print('Fold: ',i)
+        i = i+1
+    
+    MSE = [['significant',mean_squared_error(y,prediction[:,0])],
+            ['positive',mean_squared_error(y,prediction[:,1])],
+           ['negative',mean_squared_error(y,prediction[:,2])]]
+    
+    return prediction,edge_count,MSE
         
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--netmats_file",help="Path to netmats file",dest='netmats_file')
-    
+    parser.add_argument("--netmats_file",help="Path to netmats file",dest='netmats_file')   
     parser.add_argument("--traits_file",help="Path to personality traits file",dest='traits_file')
- 
+    parser.add_argument("--trait",help="Personality trait to predict, options:'N','E','A','O','C'.",dest='trait')
+    parser.add_argument("--method",help="Prediction method, options: 'LR','CPM','SVR'.",dest='method') 
     args = parser.parse_args()
-    print('loading data...')
+    
+    print('loading full data...')
     # connectivity matrices, 200x200 matrices in vector 810 subjects (shape 810x40000)
     netmats = np.loadtxt(args.netmats_file)
     traits = pd.read_csv(args.traits_file, index_col=0)
-    neuroticism = traits['NEOFAC_N'].to_numpy()
+    col_name = 'NEOFAC_{}'.format(args.trait)
+    trait = traits[col_name].to_numpy()
 
+    print('starting {}...'.format(args.method))
+    if (args.method == 'CPM'):
+        pred, edge_count, MSE = leave_one_out_CPM(netmats,trait)
+    if (args.method == 'LR'):
+        pred, edge_count, MSE = leave_one_out_LR(netmats,trait)
+    if (args.method == 'SVR'):
+        pred, edge_count, MSE = leave_one_out_SVR(netmats,trait)
     
-    pred, edge_count = leave_one_out_CPM(netmats,neuroticism)
-
-    MSE = [['combined',mean_squared_error(neuroticism,pred[:,0])],
-            ['positive',mean_squared_error(neuroticism,pred[:,1])],
-           ['negative',mean_squared_error(neuroticism,pred[:,2])],
-           ['multiple_reg',mean_squared_error(neuroticism,pred[:,3])]]
-
-    
-    #TODO: WRITE DATA OUT
-    print(MSE)
-    pd.DataFrame(columns = ['type','MSE']).to_csv('neuroticism_MSE.csv')
-    np.savetxt('neuroticism_edge_count.csv',edge_count)
-    np.savetxt('neuroticism_predictions.csv',pred)
+    print('writing data...')
+    print('MSE ',MSE)
+    pd.DataFrame(columns = ['type','MSE']).to_csv('{}_{}_MSE.csv'.format(col_name,args.method))
+    #np.savetxt('{}_{}_edge_count.csv'.format(col_name,args.method),edge_count)
+    np.savetxt('{}_{}_predictions.csv'.format(col_name,args.method),pred)
